97ea5bb081d699e2f92e687f2ccccc7228d6fe24
Yan Jie Jie <>
2021-04-21 10:58:45 +0000

    Checking Question 4

--- Question4.java
+++ Question4.java
@@ -29,7 +29,7 @@ public class Question4 {
       for (int x = 0; x <= i; x++) {
         System.out.print("*");
       }
-      System.out.println();
+      // System.out.println();
       i++;
     }
   }
